665b2733585ceaea45a07c8da0720baa9196cf6a
Arda Şalvarlılar <arda4salvar7@gmail.com>
2023-11-19 13:06:53 +0300

    weather app completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import TurkeyMap from 'turkey-map-react';
+import Sonuc from './components/Sonuc';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const key = "f9dba21823ae48f4cfac34cff8830e82"
+  const [city, setCity] = useState('')
+  const [response, setResponse] = useState('')
+
+useEffect(() => {
+  // Application Programming Interface
+  async function getApi() {
+    try {
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric&lang=tr`);
+      setResponse(response.data)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  if(city !== ''){
+    getApi()
+  }
+},[city])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TurkeyMap
+        hoverable={true} 
+        onHover={({plateNumber, name}) => console.log(plateNumber, name)} 
+        onClick={({name}) => setCity(name)}
+      />
+      {response && <Sonuc response = {response} />}
     </div>
   );
 }
